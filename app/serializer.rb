f42aca570d33697311f13d8d60e56c2c4dba6d2a
Ali Sajid <alisajidsiddique@gmail.com>
2022-09-21 19:11:46 +0500

    Create initialize method for Serialize class

--- app/serializer.rb
+++ app/serializer.rb
@@ -6,4 +6,12 @@ end
 
 class Serializer
     
+    def initialize(obj)
+        if obj.class == Comment
+            @obj = Comment.new(obj.id, obj.body, obj.title)
+        elsif
+            @obj = Post.new(obj.id, obj.title, obj.date)
+        end
+    end
+
 end
